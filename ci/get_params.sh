f846181752407132c3f54bd98a1c8b2cca0e442d
Florian Sloot <flosloot@gmx.de>
2020-05-16 16:51:02 +0200

    [CI] adapt CI scripts

--- ci/get_params.sh
+++ ci/get_params.sh
@@ -6,9 +6,10 @@ ENV_FILE_OUTPUT=$PWD/.env
 # get parameter from AWS parameter store
 getSSMParam() {
     echo ---------------
-    echo $1
+    echo "Setting ENV => $1"
     PARAM_VAL=$(aws ssm get-parameter --name janus-env-$1 --region us-east-1 | jq -r '.Parameter.Value')
     echo "$1=$PARAM_VAL" >> $ENV_FILE_OUTPUT
+    echo -e "Done \xE2\x9C\x94"
 }
 
 # split eachline at the "=" so all env's get overwritten during script
@@ -18,6 +19,8 @@ getValueForEnv() {
     getSSMParam $ENV_VAR
 }
 
+# delete if exisiting output file
+rm $ENV_FILE_OUTPUT 2> /dev/null
 # read every line from .env.example file to get each variable name
 while read line;
     do
