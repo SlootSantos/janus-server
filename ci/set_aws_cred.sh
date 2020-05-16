e9d98966429ec8e0d3290af2241c591fc70b648c
Florian Sloot <flosloot@gmx.de>
2020-05-16 17:24:49 +0200

    [CI] adapt scripts to circleci

--- ci/set_aws_cred.sh
+++ ci/set_aws_cred.sh
@@ -4,7 +4,7 @@ mkdir ~/.aws
 AWS_CRED_FILE=~/.aws/credentials
 
 echo "[default]" > $AWS_CRED_FILE
-echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> $AWS_CRED_FILE
-echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> $AWS_CRED_FILE
+echo -e "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> $AWS_CRED_FILE
+echo -e "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> $AWS_CRED_FILE
 
 cat $AWS_CRED_FILE
