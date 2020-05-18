5693d2f727e46c4964753857b6592d25ea576d6d
Florian Sloot <flosloot@gmx.de>
2020-05-18 10:24:30 +0200

    [FEAT] change healthy response for test

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -3,6 +3,7 @@ package api
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/SlootSantos/janus-server/pkg/api/auth"
 	"github.com/SlootSantos/janus-server/pkg/jam"
@@ -21,10 +22,14 @@ func Start(j *jam.Creator) {
 	http.HandleFunc(pipeline.Hook, pipeline.HandleHook)
 	http.HandleFunc(auth.Login, auth.HandleLogin)
 	http.HandleFunc("/health", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte("ALIVE"))
+		w.Write([]byte("ALIVE :)"))
 	})
 	log.Println("DONE: setting up API routes")
 	log.Println("LISTEN: :8888")
+
+	if os.Getenv("IS_ENTERPRISE") != "" {
+		log.Println("\n----\n Running properitary Janus Version including Payment options!\n----")
+	}
 
 	http.ListenAndServe(":8888", nil)
 }
