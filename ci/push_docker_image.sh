557fcdbf747ec8961ef0f10b860cef394972d62d
Florian Sloot <flosloot@gmx.de>
2020-05-19 15:13:03 +0200

    [DEPLOY] redeploy using new settings

--- ci/push_docker_image.sh
+++ ci/push_docker_image.sh
@@ -8,5 +8,5 @@ docker build -t janus/server:latest -f "${PWD}/Dockerfile" ${PWD}
 IMAGE_ID=$(docker images -q janus/server:latest)
 ECR_URL=$(aws ssm get-parameter --name /janus/env/production/ECR_URL --region us-east-1 --with-decryption | jq -r '.Parameter.Value')
 
-docker tag $IMAGE_ID $ECR_URL/janus/server:latest
-docker push $ECR_URL/janus/server:latest
+docker tag $IMAGE_ID $ECR_URL:latest
+docker push $ECR_URL:latest
