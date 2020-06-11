5190840fd13be427d70b91578cd54d7f7e74bd61
Florian Sloot <flosloot@gmx.de>
2020-06-11 17:48:00 +0200

    [DUP_HANDLING] handle possible duplicated role && function

--- pkg/stacker/http.go
+++ pkg/stacker/http.go
@@ -3,6 +3,7 @@ package stacker
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -12,6 +13,7 @@ import (
 	"github.com/SlootSantos/janus-server/pkg/session"
 	"github.com/SlootSantos/janus-server/pkg/storage"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/aws/aws-sdk-go/service/lambda"
 )
@@ -146,7 +148,7 @@ func SetThirdPartyAWSCredentials(w http.ResponseWriter, req *http.Request) {
 		Domain:    creds.Domain,
 	}
 
-	lambdaARN := setupThirdPartyAccount(req.Context(), user.ThirdPartyAWS)
+	lambdaARN, _ := setupThirdPartyAccount(req.Context(), user.ThirdPartyAWS)
 	user.ThirdPartyAWS.LambdaARN = lambdaARN
 
 	err = storage.Store.User.Set(req.Context().Value(auth.ContextKeyUserName).(string), user)
@@ -159,46 +161,76 @@ func SetThirdPartyAWSCredentials(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func setupThirdPartyAccount(ctx context.Context, thirdPartyCreds *storage.ThirdPartyAWS) string {
+func setupThirdPartyAccount(ctx context.Context, thirdPartyCreds *storage.ThirdPartyAWS) (string, error) {
 	thirdPartySession, _ := session.AWSSessionThirdParty(thirdPartyCreds.AccessKey, thirdPartyCreds.SecretKey)
 	i := iam.New(thirdPartySession)
+
+	roleName := "role-with-policy-11"
+	var roleARN string
 	role, err := i.CreateRole(&iam.CreateRoleInput{
 		Path:                     aws.String("/service-role/"),
 		AssumeRolePolicyDocument: aws.String("{\"Version\": \"2012-10-17\",\"Statement\": [{\"Effect\": \"Allow\",\"Principal\": {\"Service\": [\"lambda.amazonaws.com\",\"edgelambda.amazonaws.com\"]},\"Action\": \"sts:AssumeRole\"}]}"),
-		RoleName:                 aws.String("role-with-policy-11"),
+		RoleName:                 aws.String(roleName),
 	})
-	log.Printf("%+v", role)
-	log.Printf("%+v", err)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case iam.ErrCodeEntityAlreadyExistsException:
+				log.Printf("User: already has role named: %s", roleName)
+				r, _ := i.GetRole(&iam.GetRoleInput{
+					RoleName: aws.String(roleName),
+				})
+				roleARN = *r.Role.Arn
+			default:
+				log.Println("Could not handle AWS err", err.Error())
+				return "", fmt.Errorf("Can not create role for AWS account: %s", err.Error())
+			}
+		}
+	} else {
+		roleARN = *role.Role.Arn
+		i.PutRolePolicy(&iam.PutRolePolicyInput{
+			PolicyName:     aws.String("stackers-lambda-exec-policy"),
+			PolicyDocument: aws.String("{\"Version\": \"2012-10-17\", \"Statement\": [ { \"Effect\": \"Allow\", \"Action\": [ \"logs:CreateLogGroup\", \"logs:CreateLogStream\", \"logs:PutLogEvents\" ], \"Resource\": [ \"arn:aws:logs:*:*:*\" ] } ] }"),
+			RoleName:       aws.String(roleName),
+		})
 
-	pol, err := i.PutRolePolicy(&iam.PutRolePolicyInput{
-		PolicyName:     aws.String("stackers-lambda-exec-policy"),
-		PolicyDocument: aws.String("{\"Version\": \"2012-10-17\", \"Statement\": [ { \"Effect\": \"Allow\", \"Action\": [ \"logs:CreateLogGroup\", \"logs:CreateLogStream\", \"logs:PutLogEvents\" ], \"Resource\": [ \"arn:aws:logs:*:*:*\" ] } ] }"),
-		RoleName:       aws.String(*role.Role.RoleName),
-	})
+		time.Sleep(time.Second * 60)
+	}
 
-	log.Printf("%+v", pol)
-	log.Printf("%+v", err)
-
-	time.Sleep(time.Second * 60)
-
+	var functionARN string
 	lam := lambda.New(thirdPartySession)
 	res, err := lam.CreateFunction(&lambda.CreateFunctionInput{
 		FunctionName: aws.String("stackers-handler-routing-2"),
 		Handler:      aws.String("index.handler"),
 		Runtime:      aws.String("nodejs12.x"),
-		Role:         aws.String(*role.Role.Arn),
+		Role:         aws.String(roleARN),
 		Code: &lambda.FunctionCode{
 			S3Bucket: aws.String("stackers.io-lambda-public-functions"),
 			S3Key:    aws.String("exx.zip"),
 		},
 	})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case lambda.ErrCodeResourceConflictException:
+				log.Printf("User: already has function named: %s", "stackers-handler-routing-2")
+				lam.GetFunction(&lambda.GetFunctionInput{
+					FunctionName: aws.String("stackers-handler-routing-2"),
+				})
+			default:
+				log.Println("Could not handle AWS err", err.Error())
+				return "", fmt.Errorf("Can not create role for AWS account: %s", err.Error())
+			}
+		}
+	} else {
+		functionARN = *res.FunctionArn
+		lam.PublishVersion(&lambda.PublishVersionInput{
+			FunctionName: aws.String(functionARN),
+			Description:  aws.String("Stackers.io Routing Lambda Function"),
+		})
+	}
 
-	lam.PublishVersion(&lambda.PublishVersionInput{
-		FunctionName: aws.String(*res.FunctionArn),
-		Description:  aws.String("Stackers.io Routing Lambda Function"),
-	})
-
-	return *res.FunctionArn
+	return functionARN, nil
 }
 
 func setLaunchParam(ctx context.Context, isThirdParty bool) (*launchParam, error) {
